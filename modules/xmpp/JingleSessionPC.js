17ad72745ae0a65393f232ec8c18aae53c4f3868
paweldomas <pawel.domas@jitsi.org>
2017-01-25 09:58:03 -0600

    fix(TraceablePeerConnection): fix lint warnings

--- modules/xmpp/JingleSessionPC.js
+++ modules/xmpp/JingleSessionPC.js
@@ -1335,11 +1335,12 @@ export default class JingleSessionPC extends JingleSession {
             case "source-remove":
                 this.fixSourceRemoveJingle(jingle);
                 break;
-            default:
+            default: {
                 const errmsg = "Unknown jingle action!";
                 GlobalOnErrorHandler.callErrorHandler(errmsg);
                 logger.error(errmsg);
                 return false;
+            }
         }
 
         const sources
